84ffb64c4498c722855fc2fc5799b77742b0e186
Felipe Costa <playmomentsstudios@gmail.com>
2026-01-20 16:54:04 -0300

    Create map.js

---
+++ src/js/map.js
@@ -0,0 +1,12 @@
+// src/js/map.js
+// Stub (não quebra o app caso o mapa ainda não esteja implementado)
+(function () {
+  if (!window.SumaumaMap) {
+    window.SumaumaMap = {
+      init: function () {
+        // No-op: o mapa real será implementado depois
+        // Mantém o app funcionando (home + rotas)
+      }
+    };
+  }
+})();
